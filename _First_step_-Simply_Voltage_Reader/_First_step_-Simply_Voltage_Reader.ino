c27d2e48ee7ebff90861f0c7cf1efa35bbdf74b2
RobSanBar <DescubreYExperimenta@gmail.com>
2021-04-18 19:56:41 +0200

    Create _First_step_-Simply_Voltage_Reader.ino

---
+++ _First_step_-Simply_Voltage_Reader/_First_step_-Simply_Voltage_Reader.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(230400);
+
+  ADCSRA = 0;             // clear ADCSRA register
+  ADCSRB = 0;             // clear ADCSRB register
+  ADMUX |= (0 & 0x07);    // set A0 analog input pin
+  ADMUX |= (1 << REFS0);  // set reference voltage
+  ADMUX |= (1 << ADLAR);  // left align ADC value to 8 bits from ADCH register
+
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS0);    // 8 prescaler for 153.8 KHz
+
+  ADCSRA |= (1 << ADATE); // enable auto trigger
+  ADCSRA |= (1 << ADIE);  // enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN);  // enable ADC
+  ADCSRA |= (1 << ADSC);  // start ADC measurements
+}
+
+ISR(ADC_vect){
+  byte x = ADCH;  // read 8 bit value from ADC
+  Serial.println(x);
+}
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+}
